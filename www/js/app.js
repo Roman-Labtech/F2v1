77fd7224eb017a1e1952a6e7c1a1a84f102ac43d
Roman-Labtech <labtechno.roman@yandex.ru>
2018-01-18 13:34:19 +0300

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -40,7 +40,7 @@ var app  = new Framework7({
     helloWorld: function () {
       app.dialog.alert('Hello World!sdsd');
     },
-	    helloWorld1: function () {
+	  helloWorld1: function () {
       app.dialog.alert('Hello World1!');
     },  
   },
@@ -69,6 +69,67 @@ $$('.open-prompt').on('click', function () {
   });
 });
 
+// Prompt
+$$('.banner').on('click', function () {
+	
+    if ( window.plugins && window.plugins.AdMob ) {
+        var ad_units = {
+            ios : {
+                banner: 'ca-app-pub-3940256099942544/6300978111',       //PUT ADMOB ADCODE HERE
+                interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'  //PUT ADMOB ADCODE HERE
+            },
+            android : {
+                banner: 'ca-app-pub-3940256099942544/6300978111',       //PUT ADMOB ADCODE HERE
+                interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'  //PUT ADMOB ADCODE HERE
+            }
+        };
+        var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+
+        window.plugins.AdMob.setOptions( {
+            publisherId: admobid.banner,
+            interstitialAdId: admobid.interstitial,
+            adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,  //use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD
+            bannerAtTop: false, // set to true, to put banner at top
+            overlap: true, // banner will overlap webview 
+            offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+            isTesting: true, // receiving test ad
+            autoShow: true // auto show interstitial ad when loaded
+        });
+
+        registerAdEvents();
+        window.plugins.AdMob.createInterstitialView();  //get the interstitials ready to be shown
+        window.plugins.AdMob.requestInterstitialAd();
+		
+//display the banner
+		window.plugins.AdMob.createBannerView();
+
+    } else {
+        alert( 'admob plugin not ready' );
+    }
+	
+});
+
+
+
+//functions to allow you to know when ads are shown, etc.
+function registerAdEvents() {
+    document.addEventListener('onReceiveAd', function(){});
+    document.addEventListener('onFailedToReceiveAd', function(data){});
+    document.addEventListener('onPresentAd', function(){});
+    document.addEventListener('onDismissAd', function(){ });
+    document.addEventListener('onLeaveToAd', function(){ });
+    document.addEventListener('onReceiveInterstitialAd', function(){ });
+    document.addEventListener('onPresentInterstitialAd', function(){ });
+    document.addEventListener('onDismissInterstitialAd', function(){
+        window.plugins.AdMob.createInterstitialView();          //REMOVE THESE 2 LINES IF USING AUTOSHOW
+        window.plugins.AdMob.requestInterstitialAd();           //get the next one ready only after the current one is closed
+    });
+}
+
+
+// Add the following 2 functions and call them when you want ads to show
+
+
 
 
 
@@ -82,6 +143,7 @@ var catalogView = app.views.create('#view-catalog', {
 var settingsView = app.views.create('#view-settings', {
   url: '/settings/'
 });
+
 
 
 // Login Screen Demo
