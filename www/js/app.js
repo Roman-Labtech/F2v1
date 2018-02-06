772dff3e6f3d91245396387775d5c8a7de2a6cb0
Roman-Labtech <labtechno.roman@yandex.ru>
2018-02-06 13:09:22 +0300

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -84,31 +84,27 @@ $$('.open-prompt').on('click', function () {
 $$('.banner').on('click', function () {
 	alert("fff");
 var admobid = {};
-// select the right Ad Id according to platform
-if( /(android)/i.test(navigator.userAgent) ) { 
-    admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+    if( /(android)/i.test(navigator.userAgent) ) { // for android & amazon-fireos 
+    admobid = {
+        //banner: 'ca-app-pub-xxx/xxx', // or DFP format "/6253334/dfp_example_ad" 
+        interstitial: 'ca-app-pub-9609087637503923/1710134090'
     };
-} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) s
-    admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios 
+    admobid = {
+        banner: 'ca-app-pub-xxx/zzz', // or DFP format "/6253334/dfp_example_ad" 
+        interstitial: 'ca-app-pub-xxx/kkk'
     };
-} else {
-    admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+} else { // for windows phone 
+    admobid = {
+        banner: 'ca-app-pub-xxx/zzz', // or DFP format "/6253334/dfp_example_ad" 
+        interstitial: 'ca-app-pub-xxx/kkk'
     };
 }
-
-AdMob.createBanner( {
-	adId:admobid.banner, 
-	position:AdMob.AD_POSITION.BOTTOM_CENTER, 
-	autoShow:true} );
+if(AdMob)
+{
+    AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+}
 	
-	
-//display the banner
 			
 });
 
